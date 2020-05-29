13d89985a54382309486a5cda2632af6d3155f1e
alisrour37 <ali.srour37@gmail.com>
2020-05-30 01:43:15 +0300

    finalized login/register ui

--- Frontend/src/views/Login.js
+++ Frontend/src/views/Login.js
@@ -1,146 +1,167 @@
-import React from 'react';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import {useState} from 'react';
 import axios from 'axios';
-
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-
-export default class Login extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            fisrtName: '',
-            lastName: '',
-        };
-    }
-
-
-
-    handleEmail = event => {
-        this.setState({ email: event.target.value });
-    }
-
-    handlePassword = event => {
-        this.setState({ password: event.target.value });
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-
-        let user = {
-            email: this.state.email,
-            password: this.state.password
-        };
-
-        axios.post('http://localhost:8000/api/auth/login', user)
-            .then(res => {
-                localStorage.setItem('access_token', res.data.access_token);
-                localStorage.setItem('token_type', res.data.token_type);
-                localStorage.setItem('isLogged', true);
-                localStorage.setItem('user_id', res.data.user_id); 
-                this.props.history.push('/admin/dashboard')
-            });
-    };
-
-    handleRegister = e => {
-        this.props.history.push('/register')
-    }
-
-    render() {
-        var useStyle = makeStyles(theme => ({
-            paper: {
-                marginTop: theme.spacing(8),
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            },
-            form: {
-                width: '100%', // Fix IE 11 issue.
-                marginTop: theme.spacing(1),
-            },
-            submit: {
-                margin: theme.spacing(3, 0, 2),
-            },
-
-        }));
-        return (
-            <div>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <div className={useStyle.paper}>
-                        <img src='/logo.png' alt=""></img>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                    </Typography>
-                        <form className={useStyle.form} noValidate onSubmit={this.handleSubmit}>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                onChange={this.handleEmail}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type='password'
-                                id="password"
-                                autoComplete="current-password"
-                                onChange={this.handlePassword}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={useStyle.submit}
-                            >
-                                Sign In
-                        </Button>
-                            <Grid container>
-                                <Grid item>
-                                    <Link onClick={this.handleRegister} variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    </div>
-                </Container>
-            </div>
-        );
-    }
-
-
+import Background from 'assets/img/background.jpg'
+import Logo from 'assets/img/newlogo.png';
+import { useHistory } from 'react-router-dom';
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: `url(${Background})`,
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
+export default function Login() {
+  const classes = useStyles();
+  const [firstName, setfirstName] = useState();
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+  const [lastName, setlastName] = useState();
+  const history = useHistory();
+  const handleEmail = (event) => {
+    setemail(event.target.value );
+  };
 
+  const handlePassword = (event) => {
+    setpassword(event.target.value);
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    let user = {
+      email: email,
+      password: password,
+    };
 
+    axios.post("http://localhost:8000/api/auth/login", user).then((res) => {
+      localStorage.setItem("access_token", res.data.access_token);
+      localStorage.setItem("token_type", res.data.token_type);
+      localStorage.setItem("isLogged", true);
+      localStorage.setItem("user_id", res.data.user_id);
+      history.push("/admin/dashboard");
+    });
+  };
 
-
-
-
-
+  const handleRegister = (e) => {
+    history.push("/register");
+  };
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+  
+          <img src={Logo} alt=""></img>
+  
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleEmail}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handlePassword}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              
+              <Grid item>
+                <Link onClick={handleRegister} variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+             
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
