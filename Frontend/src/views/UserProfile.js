18eff9495c6ba2adf1827e2dd29c769c98907dcb
alisrour37 <ali.srour37@gmail.com>
2020-06-10 23:35:57 +0300

    userprofile ui

--- Frontend/src/views/UserProfile.js
+++ Frontend/src/views/UserProfile.js
@@ -17,7 +17,7 @@ import {
 } from "reactstrap";
 import { setOriginalNode } from "typescript";
 
-function UserProfile() {
+export default function UserProfile() {
   
 const [firstname,setfirstname] = useState();
 const [lastname,setlastname] = useState();
@@ -49,17 +49,17 @@ useEffect(() => {
     })
 }, []);
 
-handleImage=(event)=>{
+const handleImage=(event)=>{
   setimage(event.target.files[0]);
   setupload('true')
 }
-handleSubmit = (event) =>{
+const handleSubmit = (event) =>{
   event.preventDefault();
   let request = new FormData();
   request.append('image',image);
     request.append('user_id', user_id);
-    request.append('first_name',firstName);
-    request.append('last_name',lastName);
+    request.append('first_name',firstname);
+    request.append('last_name',lastname);
     request.append('username',username)
     request.append('about', about)
     request.append('upload',upload)
@@ -68,45 +68,26 @@ handleSubmit = (event) =>{
       headers: { Authorization: "Bearer " + localStorage.getItem('access_token') }
     })
 }
+const imgsrc = "http://localhost:8000/image/" + profile + ".png";
     return (
       <>
         <div className="content">
           <Row>
+            
             <Col md="8">
               <Card>
                 <CardHeader>
                   <h5 className="title">Edit Profile</h5>
                 </CardHeader>
                 <CardBody>
-                  <Form>
-                    <Row>
-                      <Col className="pr-md-1" md="5">
-                        <FormGroup>
-                          <label>Company (disabled)</label>
-                          <Input
-                            defaultValue="Creative Code Inc."
-                            disabled
-                            placeholder="Company"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="px-md-1" md="3">
-                        <FormGroup>
-                          <label>Username</label>
-                          <Input
-                            defaultValue="michael23"
-                            placeholder="Username"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="pl-md-1" md="4">
-                        <FormGroup>
-                          <label htmlFor="exampleInputEmail1">
-                            Email address
+                  <Form encType="multipart/form-data" onSubmit={handleSubmit}>
+                  <Row>
+                      <Col md="12">
+                        <FormGroup >
+                          <label htmlFor="exampleInputEmail1" >
+                            Username
                           </label>
-                          <Input placeholder="mike@email.com" type="email" />
+                          <Input id='Us' placeholder={placeholderUs} type="text" onChange={setusername}  />
                         </FormGroup>
                       </Col>
                     </Row>
@@ -115,9 +96,10 @@ handleSubmit = (event) =>{
                         <FormGroup>
                           <label>First Name</label>
                           <Input
-                            defaultValue="Mike"
-                            placeholder="Company"
+                            id='FN'
                             type="text"
+                            onChange={setfirstname}
+                            placeholder={placeholderFN}
                           />
                         </FormGroup>
                       </Col>
@@ -125,50 +107,21 @@ handleSubmit = (event) =>{
                         <FormGroup>
                           <label>Last Name</label>
                           <Input
-                            defaultValue="Andrew"
-                            placeholder="Last Name"
+                            id='LN'
+                            onChange={setlastname}
                             type="text"
+                            placeholder={placeholderLN}
                           />
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
                       <Col md="12">
-                        <FormGroup>
-                          <label>Address</label>
-                          <Input
-                            defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                            placeholder="Home Address"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="4">
-                        <FormGroup>
-                          <label>City</label>
-                          <Input
-                            defaultValue="Mike"
-                            placeholder="City"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="px-md-1" md="4">
-                        <FormGroup>
-                          <label>Country</label>
-                          <Input
-                            defaultValue="Andrew"
-                            placeholder="Country"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="pl-md-1" md="4">
-                        <FormGroup>
-                          <label>Postal Code</label>
-                          <Input placeholder="ZIP Code" type="number" />
+                        <FormGroup >
+                          <label htmlFor="exampleInputEmail1" >
+                            Email address
+                          </label>
+                          <Input placeholder={placeholderEmail} type="email" disabled />
                         </FormGroup>
                       </Col>
                     </Row>
@@ -178,11 +131,23 @@ handleSubmit = (event) =>{
                           <label>About Me</label>
                           <Input
                             cols="80"
-                            defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                            placeholder="Here can be your description"
+                            placeholder={placeholderAB}
+                            onChange={setabout}
                             rows="4"
+                            id='AB'
                             type="textarea"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md="1">
+                        <FormGroup>
+                          <label>Profile Picture</label>
+                          <img src={uploadIcon} alt="" ></img>
+                          <Input
+                            type="file"
+                            onChange={handleImage}
                           />
                         </FormGroup>
                       </Col>
@@ -190,12 +155,13 @@ handleSubmit = (event) =>{
                   </Form>
                 </CardBody>
                 <CardFooter>
-                  <Button className="btn-fill" color="primary" type="submit">
+                  <Button className="btn-fill" color="primary" type="submit" onClick={handleSubmit}>
                     Save
                   </Button>
                 </CardFooter>
               </Card>
             </Col>
+
             <Col md="4">
               <Card className="card-user">
                 <CardBody>
@@ -205,42 +171,24 @@ handleSubmit = (event) =>{
                     <div className="block block-two" />
                     <div className="block block-three" />
                     <div className="block block-four" />
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
                       <img
-                        alt="..."
-                        className="avatar"
-                        src={require("assets/img/emilyz.jpg")}
+                        src={imgsrc}
                       />
-                      <h5 className="title">Mike Andrew</h5>
-                    </a>
-                    <p className="description">Ceo/Co-Founder</p>
+                      <h2 className="title">{username}</h2>
                   </div>
-                  <div className="card-description">
-                    Do not be scared of the truth because we need to restart the
-                    human foundation in truth And I love you like Kanye loves
-                    Kanye I love Rick Owens’ bed design but the back is...
+                  <div className="card-description" style={{ color: 'white', fontSize: 18}}>
+                    {about}
                   </div>
                 </CardBody>
-                <CardFooter>
-                  <div className="button-container">
-                    <Button className="btn-icon btn-round" color="facebook">
-                      <i className="fab fa-facebook" />
-                    </Button>
-                    <Button className="btn-icon btn-round" color="twitter">
-                      <i className="fab fa-twitter" />
-                    </Button>
-                    <Button className="btn-icon btn-round" color="google">
-                      <i className="fab fa-google-plus" />
-                    </Button>
-                  </div>
-                </CardFooter>
+
               </Card>
             </Col>
+            
           </Row>
         </div>
       </>
     );
   }
-}
 
-export default UserProfile;
+
+
